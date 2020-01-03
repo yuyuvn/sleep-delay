4b99581f10c1e6bcfaa97ebc5470a039a6805994
Clicia Scarlet <yuyuvn@mac.com>
2020-01-03 17:05:39 +0900

    First commit

---
+++ Timer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SleepTimer
+{
+    public enum StatusType { StandBy, Running }
+
+    class Timer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string TimeLeft { get; set; }
+        public string ActionName
+        {
+            get
+            {
+                if (status == StatusType.StandBy) return "Run";
+                else return "Exit";
+            }
+        }
+        public StatusType status { get; private set; }
+
+        public Timer(uint defaultDelay)
+        {
+            TimeLeft = defaultDelay.ToString();
+        }
+
+        public void Toogle()
+        {
+            if (status == StatusType.StandBy) Start();
+            else Stop();
+            if (this.PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs("ActionName");
+                this.PropertyChanged(this, e);
+            }
+        }
+
+        private void Start()
+        {
+            status = StatusType.Running;
+            System.Threading.Tasks.Task.Factory.StartNew(async () =>
+            {
+                Thread.Sleep(Int32.Parse(TimeLeft) *60000);
+                HttpClient client = new HttpClient();
+                var values = new Dictionary<string, string>
+                {
+                    { "value1", "" },
+                    { "value2", "" },
+                    { "value3", "" }
+                };
+                var content = new FormUrlEncodedContent(values);
+                await client.PostAsync(Properties.Settings.Default.Endpoint, content);
+                Process.Start("shutdown", "/f /s /t 0");
+            });
+        }
+
+        private void Stop()
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+    }
+}
